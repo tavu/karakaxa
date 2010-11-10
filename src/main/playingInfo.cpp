88fcbd84cb120e2b0fec18815b967ebb9099fba4
niktavu <niktavu@e3e142fc-87cb-472b-878a-498f94244814>
2010-11-10 11:57:11 +0000

    git-svn-id: https://linuxplayer.svn.sourceforge.net/svnroot/linuxplayer/branches/branche1@27 e3e142fc-87cb-472b-878a-498f94244814

--- src/main/playingInfo.cpp
+++ src/main/playingInfo.cpp
@@ -4,67 +4,59 @@
 #include<KToolBar>
 #include<KIcon>
 #define ICONZISE QSize(35,35)
-#include<QGridLayout>
 playingInfo::playingInfo(QWidget *parent)
         :QWidget(parent)
 {
-     cover=new player::coverWidget(this);
-     cover->setSize(90,110);
 
-//     QHBoxLayout *hLayout=new QHBoxLayout();
-// //     QVBoxLayout *vLayout2=new QVBoxLayout();
-//     QVBoxLayout *vLayout=new QVBoxLayout();
+//      file=0;
+//      setSizePolicy(QSizePolicy::Ignored,QSizePolicy::Ignored);
+    cover=new player::coverWidget(this);
+    cover->setSize(90,110);
 
-//     hLayout->addWidget(cover);
-    
-//     vLayout1->addLayout(hLayout);
-    
-    titleT=new scrolText("AAAAA",this);
+//      l=new QLabel(this);
+
+
+
+    QHBoxLayout *hLayout=new QHBoxLayout();
+    QVBoxLayout *vLayout=new QVBoxLayout();
+
+
+    titleT=new scrolText(this);
     titleT->setBold(true);
     albumT= new scrolText(this);
     artistT= new scrolText(this);
     commentT= new scrolText(this);
+//      titleL=new QLabel(this);
+//      titleL->setWordWrap(true);
+//      albumL=new QLabel(this);
+//      artistL=new QLabel(this);
+//      commentL=new QLabel(this);
 
-//     vLayout->addWidget(cover);
-//     vLayout->addWidget(commentT);
-//     vLayout->addWidget(titleT);
-//     vLayout->addWidget(albumT);
-//     vLayout->addWidget(artistT);
-    
+    vLayout->addWidget(titleT);
+    vLayout->addWidget(albumT);
+    vLayout->addWidget(artistT);
+    vLayout->addWidget(commentT);
 //      vLayout->addWidget(toolBar);
-    QGridLayout *layout=new QGridLayout(this);
-    layout->setContentsMargins(20,20,50,20);
-    qDebug()<<albumT->sizeHint();
-//     layout->addWidget(commentT,0,0);
-      layout->addWidget(cover,0,0,3,1);
-      layout->addWidget(titleT,0,1);
-//     layout->addWidget(cover,1,0);
-     layout->addWidget(albumT,1,0,1,2);
-     layout->addWidget(artistT,2,0,1,2);
-//     layout->addWidget(artistT,3,0);
 
+    hLayout->addWidget(cover);
+    hLayout->addLayout(vLayout);
 
-    
-//     vLayout2->addLayout(hLayout);
-//     vLayout2->addLayout(vLayout1);
-
-//     setLayout(layout);
+    setLayout(hLayout);
 //      setFixedHeight(50);
-//     setMinimumWidth(200);
+    setMinimumWidth(200);
 //      update("/media/D/music/BLACK n WHITE - Escucha Lo/02 Escucha Lo.mp3");
-     connect(&engine,SIGNAL(trackChanged(QString) ),this,SLOT(update(QString)) );
+    connect(&engine,SIGNAL(trackChanged(QString) ),this,SLOT(update(QString)) );
 }
 
 void playingInfo::update(QString path)
 {
-
     track=npList.getPlayingTrack();
     getInfo();
 }
 
 void playingInfo::getInfo()
 {
-    if (track.isNull() )
+    if (track.isNull())
     {
         qDebug()<<"playingInfo: can't get informarion file is null";
         return;
@@ -75,8 +67,7 @@ void playingInfo::getInfo()
     artistT->setText(track->tag(LEAD_ARTIST).toString() );
     commentT->setText(track->tag(COMMENT).toString() );
 
-//     cover->setCover(track->cover() );
-    
+    cover->setCover(track->cover() );
 //      l->setPixmap(file->cover() );
 
 }
