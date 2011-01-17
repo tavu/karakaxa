44d2485b9a847add8f6924f4151dd4011dac7b0c
niktavu <niktavu@e3e142fc-87cb-472b-878a-498f94244814>
2011-01-17 12:27:31 +0000

    git-svn-id: https://linuxplayer.svn.sourceforge.net/svnroot/linuxplayer/trunk@47 e3e142fc-87cb-472b-878a-498f94244814

--- src/content/getUrl.h
+++
@@ -1,9 +0,0 @@
-#ifndef TRACKURL_H
-#define TRACKURL_H
-
-class trackUrl
-{
-public:
-    virtual url(int row)=0;
-};
-#endif
--- src/content/trackUrl.h
+++
@@ -1,9 +0,0 @@
-#ifndef TRACKURL_H
-#define TRACKURL_H
-#include <kurl.h>
-class trackUrl
-{
-public:
-    virtual KUrl url( int row) const=0;
-};
-#endif
