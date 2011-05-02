4d5d9fe645edc370dd59c8cea21915575c902019
niktavu <niktavu@e3e142fc-87cb-472b-878a-498f94244814>
2011-05-02 14:23:06 +0000

    git-svn-id: https://linuxplayer.svn.sourceforge.net/svnroot/linuxplayer/trunk@72 e3e142fc-87cb-472b-878a-498f94244814

--- install.sh
+++ install.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 appname=player
+libname=lib$appname.so
+
+./configure
+
+make
 
 mkdir -p /usr/share/kde4/apps/$appname
 
 cp -R  data/* /usr/share/kde4/apps/$appname
 
-cp -R 
+cp -R target/$appname /usr/bin
+
+cp  target/$libname /usr/lib
+
+sudo ln -s /usr/lib/$libname /usr/lib/$libname.1
+
+sudo ln -s /usr/lib/$libname /usr/lib/$libname.1.0
