337d7796491bde1bb3fa99c7f18e7bc279bdb2a9
niktavu <niktavu@e3e142fc-87cb-472b-878a-498f94244814>
2011-05-18 09:19:44 +0000

    git-svn-id: https://linuxplayer.svn.sourceforge.net/svnroot/linuxplayer/trunk@75 e3e142fc-87cb-472b-878a-498f94244814

--- install.sh
+++ install.sh
@@ -6,14 +6,20 @@ libname=lib$appname.so
 
 make
 
-mkdir -p /usr/share/kde4/apps/$appname
-
-cp -R  data/* /usr/share/kde4/apps/$appname
-
-cp -R target/$appname /usr/bin
+rm /usr/lib/$libname.1 /usr/lib/$libname.1.0
 
 cp  target/$libname /usr/lib
 
 sudo ln -s /usr/lib/$libname /usr/lib/$libname.1
 
 sudo ln -s /usr/lib/$libname /usr/lib/$libname.1.0
+
+
+
+mkdir -p /usr/share/kde4/apps/$appname
+
+cp -R  data/* /usr/share/kde4/apps/$appname
+
+cp -R sql /usr/share/kde4/apps/$appname
+
+cp -R target/$appname /usr/bin
