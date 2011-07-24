4a64f36759be6034206e855c4077ed0e7c7bd021
niktavu <niktavu@e3e142fc-87cb-472b-878a-498f94244814>
2011-07-24 16:17:13 +0000

    git-svn-id: https://linuxplayer.svn.sourceforge.net/svnroot/linuxplayer/trunk@103 e3e142fc-87cb-472b-878a-498f94244814

---
+++ src/player/content/folder/folderEditorFactory.cpp
@@ -0,0 +1,50 @@
+#include"folderEditorFactory.h"
+
+bool folderEditorFactory::setModelData(views::tagEditor* editor, QAbstractItemModel* model, const QModelIndex& index, const QModelIndexList& list)
+{
+//     return tagEditorFactory::setModelData(editor,model,index,list);
+    if(editor->tag()==audioFiles::RATING)
+    {
+	   model->setData(index,editor->value());
+	   return true;
+    }
+  
+    QLinkedList<audioFiles::audioFile>l;
+    
+    if(list.isEmpty() )
+    {
+	   model->setData(index,editor->value() );
+    }
+    else
+    {
+	   foreach(QModelIndex in,list)
+	   {
+		  QUrl u=in.data(URL_ROLE).toUrl();
+		  if(u.isValid() )
+		  {
+			 l<<audioFiles::audioFile(u.toLocalFile() );
+		  }
+	   }
+	   
+	   editThr *thr=new editThr(editor->tag(),editor->value(),l,this);
+	   connect(thr,SIGNAL(finished()),this,SLOT(thrFinished() ) );
+	   thr->start();
+    }
+    
+    return true;
+}
+
+void folderEditorFactory::thrFinished()
+{    
+     delete sender();
+	view->viewport()->update();
+}
+
+
+void editThr::run()
+{
+    foreach(audioFiles::audioFile f,fileList)
+    {
+ 	   f.setTag(tag,value);
+    }
+}
