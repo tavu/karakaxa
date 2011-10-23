cbbcf81129a4092c589260309708d92b68b9a92b
niktavu <niktavu@e3e142fc-87cb-472b-878a-498f94244814>
2011-10-23 13:34:24 +0000

    git-svn-id: https://linuxplayer.svn.sourceforge.net/svnroot/linuxplayer/trunk@126 e3e142fc-87cb-472b-878a-498f94244814

---
+++ src/player/content/playlist/sortProxyModel.cpp
@@ -0,0 +1,36 @@
+#include"sortProxyModel.h"
+#include<items.h>
+#include<core.h>
+
+bool sortProxyModel::lessThan(const QModelIndex& left, const QModelIndex& right) const
+{        
+    QString leftS=left.data(Qt::DisplayRole).toString();
+    QString rightS=right.data(Qt::DisplayRole).toString();
+    
+    int  leftT=left.data(standardItem::typeRole).toInt();
+    int  rightT=right.data(standardItem::typeRole).toInt();
+    
+    if(leftT<rightT )
+    {
+	return true;
+    }
+    if(leftT>rightT )
+    {
+	return false;
+    }
+    if(QString::compare(leftS,rightS,Qt::CaseInsensitive ) <0 )
+    {
+ 	return true;
+    }
+    else
+    {
+	return false;
+    }
+//     return QSortFilterProxyModel::lessThan(left, right);
+}
+
+void sortProxyModel::fetchMore(const QModelIndex& parent)
+{    
+    QSortFilterProxyModel::fetchMore(parent);
+    sort(0,Qt::AscendingOrder);
+}
